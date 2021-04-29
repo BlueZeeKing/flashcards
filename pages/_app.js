25d7c6554429e66c713863ad694f2f35050c0ec7
BlueZeeKing <52867955+BlueZeeKing@users.noreply.github.com>
2021-04-29 19:24:48 -0400

    fixed the vh

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
 import '../styles/globals.css'
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
+  function handler() {
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handler)
+    return () => { window.removeEventListener("resize", handler) }
+  })
+  
   return <Component {...pageProps} />
 }
 
